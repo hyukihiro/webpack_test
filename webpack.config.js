dc4875a52987e9a969d7be0f4ad8a2e2046ac804
星出 幸宏 <hoshideyukihiro@gmail.com>
2017-10-01 17:45:34 +0900

    [update] add npm scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -5,5 +6,33 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
-  }
+  },
+
+  // modules
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist'),
+    port: 8081
+  },
+
+  devtool: 'cheap-module-eval-source-map',
+
+  // plugins
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     sourceMap: true
+  //   })
+  // ]
+
 };
